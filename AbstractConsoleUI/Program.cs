c096edcc5628ec2ae8f8c4e7b6cf24618fc64740
Dongyun Huang <Dongyun Huang@DESKTOP-THC0QCI>
2023-07-12 10:37:47 +0800

    Add project files.

---
+++ AbstractConsoleUI/Program.cs
@@ -0,0 +1,38 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // DataAccess da = new DataAccess(); // Error - benefit 1
+            // SqliteDataAccess da = new SqliteDataAccess(); // OK
+            //DataAccess da = new SqliteDataAccess(); // OK  - benifit 2
+            
+
+                List<DataAccess> databases = new List<DataAccess>()
+            {
+                //new SqlDataAccess(),
+                new SqliteDataAccess()
+
+
+            };
+
+            foreach (var db in databases)
+            {
+                Console.WriteLine(db.LoadConnectionString("Demo")); // Demo virtual keyword - override
+                //db.LoadConnectionString("demo");
+                //db.LoadData("select * from table");
+                //db.LoadData("insert into table");
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DemoLibrary/DataAccess.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    //change: base class -> abstract class
+    // Benefits: 1. Does NOT allow us to instantiate it directly
+    //           2. Can create instance: BaseClass x = new CildClass();
+    //  - Blends both a base class and the interface
+
+    // when to use: (inheritance) IsA relationship ie. "X is a Y"
+    public abstract class DataAccess
+    {
+        // retrun a value: virtual - you don't have to override or can
+        public virtual string LoadConnectionString(string name)
+        {
+            Console.WriteLine("Load Connection String");
+            return "testConnectionString";
+        }
+
+        public abstract void LoadData(string sql);
+        public abstract void SaveData(string sql);
+    }
+}
